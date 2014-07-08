b6f6c019479ba9ecfc512aa7b0d14402da6650b7
Ben Boyle <benjamins.boyle@gmail.com>
2014-07-08 23:32:04 +1000

    implement routes
    - add individual views
    - load angular-route
    - define routes in js
    - update css

---
+++ app/routes.js
@@ -0,0 +1,21 @@
+// create app
+angular.module( 'waitstaff' )
+	.config(function( $routeProvider ) {
+		$routeProvider
+			.when( '/', {
+				templateUrl: 'app/views/index.html'
+			})
+
+			.when( '/new-meal', {
+				templateUrl: 'app/views/meal.html'
+			})
+
+			.when( '/my-earnings', {
+				templateUrl: 'app/views/earnings.html',
+				controller: 'earningsController'
+			})
+
+			.otherwise({ redirectTo: '/' })
+		;
+	})
+;
